0f015485d0e5eb2164fed8f0e1157b3d2f247a00
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-11-21 13:26:36 +0530

    Update Id_Clone.py

--- nexichat/modules/Id_Clone.py
+++ nexichat/modules/Id_Clone.py
@@ -69,7 +69,7 @@ async def clone_txt(client, message):
             logging.exception("Error during cloning process.")
             await mi.edit_text(f"**Invalid String Session. Please provide a valid pyrogram string session.:**\n\n**Error:** `{e}`")
     else:
-        await message.reply_text("**Provide a Pyrogram String Session after the /idclone **\n\n**Example:** `/idclone string session paste here`\n\n**Get a Pyrogram string session from here:-** [Click Here](https://telegram.tools/session-string-generator#pyrogram,user) ")
+        await message.reply_text("**Provide a Pyrogram String Session after the /idclone **\n\n**Example:** `/idclone string session paste here`\n\n**Get a Pyrogram string session from here:-** [Click Here](telethon==1.26.1) ")
 
 
 @app.on_message(filters.command(["idcloned", "clonedid"]))
