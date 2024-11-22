bf53d7c37e69f6061930bacbf75938dbdf9d9c84
𝐀ᴍʀɪᴛ 𝐑ᴀᴊ <155341830+amritraj78@users.noreply.github.com>
2024-11-22 19:21:37 +0530

    Update userbot.py

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -24,10 +24,9 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("THE_VIP_BOY")
-                await self.one.join_chat("THE_VIP_BOY_OP")
-                await self.one.join_chat("TG_FRIENDSS")
-                await self.one.join_chat("VIP_CREATORS")
+                await self.one.join_chat("ll_KINGDOM_ll")
+                await self.one.join_chat("ll_IMPERIAL_ll")
+               
 
             except:
                 pass
