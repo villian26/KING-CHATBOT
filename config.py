faad0d134a18db08dc28528a711d46b8b1e910d5
𝐀ᴍʀɪᴛ 𝐑ᴀᴊ <155341830+amritraj78@users.noreply.github.com>
2024-11-22 18:52:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,12 +11,12 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1808943146"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/THE-VIP-BOY-OP/VIP-CHATBOT")
+OWNER_ID = int(getenv("OWNER_ID", "7745014754"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/amritraj78/KING-CHATBOT")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "TG_FRIENDSS"
-UPDATE_CHNL = "VIP_CREATORS"
-OWNER_USERNAME = "THE_VIP_BOY"
+SUPPORT_GRP = "ll_KINGDOM_ll"
+UPDATE_CHNL = "ll_IMPERIAL_ll"
+OWNER_USERNAME = "ll_BRANDED_ll"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
